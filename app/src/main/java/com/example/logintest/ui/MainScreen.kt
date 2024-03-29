cb5c3cb45d2c1b98d11306bc7e72b5fbbf59e8ff
Mike Collins <fixitman.mike@outlook.com>
2024-03-29 16:51:02 -0400

    playing with the UI

--- app/src/main/java/com/example/logintest/ui/MainScreen.kt
+++ app/src/main/java/com/example/logintest/ui/MainScreen.kt
@@ -1,16 +1,32 @@
 package com.example.logintest.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.logintest.dataaccess.Reminder
 import com.example.logintest.ui.theme.LoginTestTheme
 
 
@@ -19,31 +35,56 @@ fun MainScreen(
     viewModel: MainViewModel = viewModel(MainViewModel::class.java)
 ){
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
     ){
-        viewModel.reminders.forEach {
-            if(it.reminderText.length > 0){
-                Text(
-                    text= it.reminderText,
-                    style = typography.bodyLarge
-                    )
-            }
-        }
-
-
         Button(onClick = { viewModel.getReminders() }) {
             Text(text = "Click Me", style = typography.bodyMedium)
         }
+        LazyColumn(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            items(viewModel.reminders) {
+                if(it.reminderText.length > 0){
+                    ReminderItem(it)
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+private fun ReminderItem(
+    it: Reminder
+) {
+    Card (
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(),
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()
+            .padding(28.dp)
+
+    ){
+        Text(
+            text = it.reminderText,
+            style = typography.headlineMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LoginTestTheme {
-        MainScreen()
-    }
+    LoginTestTheme { ReminderItem(it = Reminder(0,0,"","Something",""))}
 }
